c9ca8b631f90b9c6f868baaf01496e9f66243463
Charly Lopez <shadowlink1993@hotmail.com>
2023-10-25 19:20:08 -0600

    "Cambios en la programacion"

---
+++ app/src/main/java/com/example/lanzardados/Dados.kt
@@ -0,0 +1,144 @@
+package com.example.lanzardados
+
+import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+class Dados(private var imagenesDados: Array<ImageView>, private var dadosImagen: Array<Int>,
+            private var arrayLayoutDados: Array<LinearLayout>, private var dadosIndicador: TextView,
+            private var contexto: Context) {
+
+    private var dadosCantidad = 0
+    private var botonPulsado = false
+
+    fun inicializarDados(contador: Int){
+        if(contador > 5){
+            dadosCantidad = 0
+            val texto = "0 DADOS"
+            dadosIndicador.text = texto
+            return
+        }
+
+        imagenesDados[contador].visibility = View.GONE
+        inicializarDados(contador + 1)
+    }
+
+    fun visualizarDados(cantidad: Int){
+        when(cantidad){
+            0 -> {
+                arrayLayoutDados[0].visibility = View.GONE
+                arrayLayoutDados[1].visibility = View.GONE
+                arrayLayoutDados[2].visibility = View.GONE
+            }
+            in 1..2 -> {
+                arrayLayoutDados[0].layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 3F)
+
+                arrayLayoutDados[0].visibility = View.VISIBLE
+                arrayLayoutDados[1].visibility = View.GONE
+                arrayLayoutDados[2].visibility = View.GONE
+            }
+            in 3..4 -> {
+                arrayLayoutDados[0].layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1.5F)
+                arrayLayoutDados[1].layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1.5F)
+
+                arrayLayoutDados[1].visibility = View.VISIBLE
+                arrayLayoutDados[2].visibility = View.GONE
+            }
+            in 5..6 -> {
+                arrayLayoutDados[0].layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1F)
+                arrayLayoutDados[1].layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1F)
+                arrayLayoutDados[2].layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1F)
+
+                arrayLayoutDados[2].visibility = View.VISIBLE
+            }
+        }
+    }
+
+    fun agregarDados(botonPulsado: Boolean){
+        var texto = ""
+        when(dadosCantidad){
+            0 -> {
+                dadosCantidad += 1
+                texto = "$dadosCantidad DADO"
+            }
+            in 1..5 -> {
+                dadosCantidad += 1
+                texto = "$dadosCantidad DADOS"
+            }
+            6 -> {
+                texto = "$dadosCantidad DADOS"
+            }
+        }
+
+        dadosIndicador.text = texto
+        if(botonPulsado){
+            imagenesDados[dadosCantidad - 1].visibility = View.VISIBLE
+            visualizarDados(dadosCantidad)
+        }
+    }
+
+    fun quitarDados(botonPulsado: Boolean){
+        var texto = "0 DADOS"
+        when(dadosCantidad){
+            1 -> {
+                dadosCantidad = 0
+            }
+            2 -> {
+                dadosCantidad -= 1
+                texto = "$dadosCantidad DADO"
+
+            }
+            in 3..6 -> {
+                dadosCantidad -= 1
+                texto = "$dadosCantidad DADOS"
+            }
+        }
+
+        dadosIndicador.text = texto
+        if(botonPulsado){
+            imagenesDados[dadosCantidad].visibility = View.GONE
+            visualizarDados(dadosCantidad)
+        }
+    }
+
+    fun lanzarDados(cantidad: Int, contador: Int){
+        if(dadosCantidad == 0){
+            Toast.makeText(contexto, "Seleccione la cantidad de dados a lanzar", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        lanzamiento(cantidad, contador)
+        visualizarDados(cantidad)
+        botonPulsado = true
+    }
+
+    private fun lanzamiento(cantidad: Int, contador: Int){
+        if (cantidad != 0) {
+            val aleatorio = Random.nextInt(0..5)
+            imagenesDados[contador].setImageResource(dadosImagen[aleatorio])
+            imagenesDados[contador].visibility = View.VISIBLE
+            lanzarDados(cantidad - 1, contador + 1)
+        }
+    }
+
+    fun getDadosCantidad(): Int {
+        return dadosCantidad
+    }
+
+    fun setDadosCantidad(cantidad: Int){
+        dadosCantidad = cantidad
+    }
+
+    fun setBotonPulsado(boolean: Boolean){
+        botonPulsado = boolean
+    }
+
+    fun getBotonPulsado(): Boolean {
+        return botonPulsado
+    }
+}
--- app/src/main/java/com/example/lanzardados/MainActivity.kt
+++ app/src/main/java/com/example/lanzardados/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.lanzardados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
-import kotlin.random.Random
-import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dado1: ImageView
@@ -18,8 +14,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dado4: ImageView
     private lateinit var dado5: ImageView
     private lateinit var dado6: ImageView
+
     private lateinit var botonAgregar: ImageView
-    private lateinit var botonRestar: ImageView
+    private lateinit var botonQuitar: ImageView
 
     private lateinit var botonLanzarDados: Button
     private lateinit var dadosIndicador: TextView
@@ -28,14 +25,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var parDados2: LinearLayout
     private lateinit var parDados3: LinearLayout
 
-    private lateinit var dados: Array<ImageView>
-    private var dadosImagen = arrayOf(R.drawable.dice_one, R.drawable.dice_two,
+    private var arrayDadosImagen = arrayOf(R.drawable.dice_one, R.drawable.dice_two,
         R.drawable.dice_three, R.drawable.dice_four,
         R.drawable.dice_five, R.drawable.dice_six)
-
-    private lateinit var texto: String
-    private var dadosCantidad = 0
-    private var botonPulsado = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         dado6 = findViewById(R.id.dado6)
 
         botonAgregar = findViewById(R.id.agregarBoton)
-        botonRestar = findViewById(R.id.restarBoton)
+        botonQuitar = findViewById(R.id.quitarBoton)
 
         botonLanzarDados = findViewById(R.id.botonLanzarDados)
         dadosIndicador = findViewById(R.id.dadosIndicador)
@@ -58,122 +50,25 @@ class MainActivity : AppCompatActivity() {
         parDados2 = findViewById(R.id.parDados2)
         parDados3 = findViewById(R.id.parDados3)
 
-        visualizarDados(dadosCantidad)
+        val arrayDados = arrayOf(dado1, dado2, dado3, dado4, dado5, dado6)
+        val arrayLayoutDados = arrayOf(parDados1, parDados2, parDados3)
 
-        dados = arrayOf(dado1, dado2, dado3, dado4, dado5, dado6)
-        dados[0].visibility = View.GONE
-        dados[1].visibility = View.GONE
-        dados[2].visibility = View.GONE
-        dados[3].visibility = View.GONE
-        dados[4].visibility = View.GONE
-        dados[5].visibility = View.GONE
-
-        texto = "$dadosCantidad DADOS"
-        dadosIndicador.text = texto
+        val dados = Dados(arrayDados, arrayDadosImagen, arrayLayoutDados, dadosIndicador, this)
+        dados.setBotonPulsado(false)
+        dados.setDadosCantidad(0)
+        dados.inicializarDados(0)
+        dados.visualizarDados(0)
 
         botonLanzarDados.setOnClickListener {
-            if(dadosCantidad == 0){
-                Toast.makeText(this, "Seleccione la cantidad de dados a lanzar", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                lanzarDados(dadosCantidad, 0)
-                visualizarDados(dadosCantidad)
-                botonPulsado = true
-            }
+            dados.lanzarDados(dados.getDadosCantidad(), 0)
         }
 
         botonAgregar.setOnClickListener {
-            when(dadosCantidad){
-                0 -> {
-                    dadosCantidad += 1
-                    texto = "$dadosCantidad DADO"
-                    dadosIndicador.text = texto
-                }
-                in 1..5 -> {
-                    dadosCantidad += 1
-                    texto = "$dadosCantidad DADOS"
-                    dadosIndicador.text = texto
-                }
-                6 -> {
-                    texto = "$dadosCantidad DADOS"
-                    dadosIndicador.text = texto
-                }
-            }
-
-            if(botonPulsado){
-                dados[dadosCantidad - 1].visibility = View.VISIBLE
-
-                visualizarDados(dadosCantidad)
-            }
+            dados.agregarDados(dados.getBotonPulsado())
         }
 
-        botonRestar.setOnClickListener {
-            when(dadosCantidad){
-                1 -> {
-                    dadosCantidad = 0
-                    texto = "0 DADOS"
-                    dadosIndicador.text = texto
-                }
-                2 -> {
-                    dadosCantidad -= 1
-                    texto = "$dadosCantidad DADO"
-                    dadosIndicador.text = texto
-                }
-                in 3..6 ->{
-                    dadosCantidad -= 1
-                    texto = "$dadosCantidad DADOS"
-                    dadosIndicador.text = texto
-                }
-            }
-
-            if(botonPulsado){
-                dados[dadosCantidad].visibility = View.GONE
-
-                visualizarDados(dadosCantidad)
-            }
-        }
-    }
-
-    private fun lanzarDados(cantidad: Int, contador: Int){
-
-        if (cantidad != 0) {
-            val aleatorio = Random.nextInt(0..5)
-            dados[contador].setImageResource(dadosImagen[aleatorio])
-            dados[contador].visibility = View.VISIBLE
-            lanzarDados(cantidad - 1, contador + 1)
-        }
-    }
-
-    private fun visualizarDados(cantidad: Int){
-        println(cantidad)
-        when(cantidad){
-            0 -> {
-                parDados1.visibility = View.GONE
-                parDados2.visibility = View.GONE
-                parDados3.visibility = View.GONE
-            }
-            in 1..2 -> {
-                parDados1.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 3F)
-
-                parDados1.visibility = View.VISIBLE
-                parDados2.visibility = View.GONE
-                parDados3.visibility = View.GONE
-            }
-
-            in 3..4 -> {
-                parDados1.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1.5F)
-                parDados2.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1.5F)
-
-                parDados2.visibility = View.VISIBLE
-                parDados3.visibility = View.GONE
-            }
-            in 5..6 -> {
-                parDados1.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1F)
-                parDados2.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1F)
-                parDados2.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1F)
-
-                parDados3.visibility = View.VISIBLE
-            }
+        botonQuitar.setOnClickListener {
+            dados.quitarDados(dados.getBotonPulsado())
         }
     }
 }
